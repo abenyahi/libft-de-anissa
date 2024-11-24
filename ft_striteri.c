615ad019f131196a17436ed5073e4490ea5af363
Anissa Benyahia <abenyahi@c2r9p1.42lehavre.fr>
2024-11-24 19:14:49 +0100

    Auto-Save

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,15 +6,12 @@
 /*   By: abenyahi <abenyahi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 17:49:30 by abenyahi          #+#    #+#             */
-/*   Updated: 2024/11/24 17:54:45 by abenyahi         ###   ########.fr       */
+/*   Updated: 2024/11/24 19:14:10 by abenyahi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-void	charupper(unsigned int index, char *charar)
-{
-	charar[index] = 'a';
-}
+
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	int	index;
@@ -27,9 +24,9 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 	}
 	
 }
-int	main(void)
+/*int	main(void)
 {
 	char	dest[] = "test";
 	ft_striteri(dest, charupper);
 	printf("%s", dest);
-}
+}*/
